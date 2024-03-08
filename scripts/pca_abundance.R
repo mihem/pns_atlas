70a2b18ce4d89075bd33db11d65149efb774e36f
mischko <mhem@mailbox.org>
2024-03-08 19:59:25 +0100

    add PCA g_ratio, axon_diameter

--- scripts/pca_abundance.R
+++ scripts/pca_abundance.R
@@ -4,6 +4,7 @@ library(tidyverse)
 library(factoextra)
 library(FactoMineR)
 library(viridis)
+library(ggrepel)
 
 # general settings ----
 conflicts_prefer(base::setdiff)
@@ -12,13 +13,12 @@ conflicts_prefer(base::as.data.frame)
 # load preprocessed data ----
 sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"))
 ic <- qs::qread(file.path("objects", "ic.qs"))
-ic_cidp_vn_ctrl_ciap <- subset(ic, level2 %in% c("CIDP", "VN", "CTRL", "CIAP"))
 
 table(ic$level2)
 table(ic_cidp_vn_ctrl_ciap$level2)
 
 # function to plot pca of Seurat abundancies
-pcaSeurat <- function(object, label1, label2, label3) {
+pcaSeurat <- function(object, label1, label2, label3, label3_colors = NULL) {
   object_parse <- deparse(substitute(object))
   cl_size <-
     base::as.data.frame.matrix(table(object@meta.data[[label1]], object@meta.data[[label2]])) |>
@@ -34,11 +34,21 @@ pca_var_plot <-
   factoextra::fviz_pca_var(
     pca_result,
     col.var = "contrib",
-    gradient.cols = viridis::viridis(100),
+    gradient.cols = viridis::plasma(100),
     repel = TRUE,
-   select.var = list(contrib = 10)) + 
-    labs(title = "")+
-    theme_classic()
+    select.var = list(contrib = 10)
+  ) +
+  labs(
+    title = "",
+    x = "PC1", y = "PC2"
+  ) +
+  theme_classic() +
+  theme(
+    axis.text = element_blank(),
+    axis.ticks = element_blank(),
+    panel.border = element_rect(color = "black", size = 1, fill = NA),
+    aspect.ratio = 1
+  )
 
   lookup_pre <-
     data.frame(
@@ -51,31 +61,6 @@ pca_var_plot <-
     data.frame(label1 = rownames(cl_size)) |>
     left_join(lookup_pre)
 
-
-  pca_plot_ind <-
-    factoextra::fviz_pca_ind(
-      pca_result,
-      pointsize = 3,
-      pointshape = 21,
-      fill = "#E7B800",
-      col.ind = "black",
-      palette = "Set2",
-      axes.linetype = "solid"
-    )
-
-  pca_ggplot_ind <-
-    ggpubr::ggpar(
-    pca_plot_ind,
-    title = "",
-    xlab = "PC1",
-    ylab = "PC2",
-    ggtheme = theme_bw() +
-      theme(axis.title.x = element_text(size=15),
-            axis.title.y = element_text(size=15),
-            plot.title = element_text(size=25))) +
-    theme_classic() +
-    coord_fixed()
-
    pca_plot_group <-
     factoextra::fviz_pca_ind(
       pca_result,
@@ -84,12 +69,12 @@ pca_var_plot <-
       geom.ind = "point",
       fill.ind = lookup$label3,
       col.ind = "black",
-      palette = "Set2",
+      palette = label3_colors,
       addEllipses = FALSE,
       ellipse.type = "confidence",
-      legend.title = "group",
+      legend.title = label3,
       axes.linetype = "solid",
-      alpha.ind = .5
+      alpha.ind = 1
     )
 
   pca_ggplot_group <-
@@ -98,81 +83,117 @@ pca_var_plot <-
       title = "",
       xlab = "PC1",
       ylab = "PC2",
+      ticks = FALSE,
+      tickslab = FALSE,
       ggtheme = theme_classic() +
-        theme(axis.title.x = element_text(size=15),
-              axis.title.y = element_text(size=15),
-              plot.title = element_text(size=25))) +
-    # geom_point(size = 2, alpha = .5, shape = 16) + 
-    # scale_fill_manual(values = object@misc$level2_cols) + 
+        theme(
+          axis.title.x = element_text(size = 15),
+          axis.title.y = element_text(size = 15),
+          plot.title = element_text(size = 25),
+          panel.border = element_rect(color = "black", size = 1, fill = NA),
+          aspect.ratio = 1,
+        )
+    ) +
     theme(
-        panel.border = element_rect(color = "black", size = 1, fill = NA),
-        aspect.ratio = 1
-    ) 
+      axis.text = element_blank(),
+      axis.ticks = element_blank(),
+    ) +
+    geom_text(aes(label = lookup$label1), nudge_y = .3)
 
 
 if(is.numeric(lookup$label3)) {
   pca_ggplot_group <- pca_ggplot_group + viridis::scale_fill_viridis(option = "magma")
 }
 
-  pca_plots <- patchwork::wrap_plots(pca_var_plot, pca_ggplot_ind, pca_ggplot_group, ncol = 3)
-  ggsave(paste0("./results/pca/pca_", object_parse, "_", label3,  ".pdf"), width = 18, height = 6,
+  pca_plots <- patchwork::wrap_plots(pca_var_plot, pca_ggplot_group, ncol = 2)
+  ggsave(paste0("./results/pca/pca_", object_parse, "_", label3,  ".pdf"), width = 12, height = 6,
          plot = pca_plots)
 
 }
 
 debugonce(pcaSeurat)
 
-# run pca abundance function
+# run pca abundance function ----
+vn_cidp_ciap_ctrl <- subset(sc_merge, level2 %in% c("VN", "CIDP", "CIAP", "CTRL"))
+
 pcaSeurat(
-  object = sc_merge,
+  object = vn_cidp_ciap_ctrl,
   label1 = "cluster",
   label2 = "sample",
-  label3 = "level1"
+  label3 = "level2",
+  label3_colors = vn_cidp_ciap_ctrl@misc$level2_cols
 )
 
 pcaSeurat(
-  object = ic_cidp_vn_ctrl_ciap,
+  object = vn_cidp_ciap_ctrl,
+  label1 = "cluster",
+  label2 = "sample",
+  label3 = "log_axon_normal",
+)
+
+pcaSeurat(
+  object = vn_cidp_ciap_ctrl,
+  label1 = "cluster",
+  label2 = "sample",
+  label3 = "g_ratio"
+)
+
+pcaSeurat(
+  object = vn_cidp_ciap_ctrl,
+  label1 = "cluster",
+  label2 = "sample",
+  label3 = "axon_diameter"
+)
+
+vn_cidp_ciap_ctrl$incat <- as.numeric(vn_cidp_ciap_ctrl$incat)
+
+pcaSeurat(
+  object = vn_cidp_ciap_ctrl,
+  label1 = "cluster",
+  label2 = "sample",
+  label3 = "incat"
+)
+
+ic_vn_cidp_ciap_ctrl <- subset(ic, level2 %in% c("CIDP", "VN", "CTRL", "CIAP"))
+
+pcaSeurat(
+  object = ic_vn_cidp_ciap_ctrl,
   label1 = "ic_cluster",
   label2 = "sample",
-  label3 = "level2"
+  label3 = "level2",
+  label3_colors = ic@misc$level2_cols
 )
 
 pcaSeurat(
-  object = sc_merge,
-  label1 = "cluster",
+  object = ic_vn_cidp_ciap_ctrl,
+  label1 = "ic_cluster",
   label2 = "sample",
-  label3 = "level2"
+  label3 = "log_axon_normal",
 )
 
 pcaSeurat(
-  object = sc_merge_cidp_vn_ctrl_ciap,
-  label1 = "cluster",
+  object = ic_vn_cidp_ciap_ctrl,
+  label1 = "ic_cluster",
   label2 = "sample",
-  label3 = "level2"
+  label3 = "g_ratio"
 )
 
 pcaSeurat(
-  object = sc_merge,
-  label1 = "cluster",
+  object = ic_vn_cidp_ciap_ctrl,
+  label1 = "ic_cluster",
   label2 = "sample",
-  label3 = "center"
+  label3 = "axon_diameter"
 )
 
-pcaSeurat(
-  object = sc_merge,
-  label1 = "cluster",
-  label2 = "sample",
-  label3 = "log_axon_normal"
-)
-
-ic_cidp_vn_ctrl_ciap$incat <- as.numeric(ic_cidp_vn_ctrl_ciap$incat)
+ic_vn_cidp_ciap_ctrl$incat <- as.numeric(ic_vn_cidp_ciap_ctrl$incat)
 
 pcaSeurat(
-  object = ic_cidp_vn_ctrl_ciap,
+  object = ic_vn_cidp_ciap_ctrl,
   label1 = "ic_cluster",
   label2 = "sample",
   label3 = "incat"
 )
+
 
 # without scDamage
 sc_merge_no_damageSC <- subset(sc_merge, subset = cluster %in% c("damageSC"), invert = TRUE)
