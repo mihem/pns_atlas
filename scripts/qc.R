c20fa261f0d12b1f28dc5f1aa5416d61aaf1aa6e
mischko <mhem@mailbox.org>
2024-11-25 22:23:00 +0100

    document qc.R

--- scripts/qc.R
+++ scripts/qc.R
@@ -1,31 +1,35 @@
-# plot quality control metrics
+#===============================================================================
+# Quality Control Metrics Plotting
+#===============================================================================
+# Purpose: Generate and save plots for quality control metrics of single-cell RNA 
+# sequencing data.
+#===============================================================================
 
-# libraries ---
+# Load necessary libraries
 library(tidyverse)
 library(Seurat)
 library(BPCells)
 library(pals)
 
-# load preprocessed data ----
+# Load preprocessed data
 sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"), nthread = 4)
 
+# Define color palette
 my_cols_50 <- unname(Polychrome::createPalette(50, pals::cols25()))
 
+# Count cells per sample
 count_cells <- dplyr::count(sc_merge@meta.data, sample)
 
-count_genes <-
-    dplyr::tibble(feature = sc_merge@meta.data$nFeature_RNA, sample = sc_merge@meta.data$sample) |>
-    dplyr::group_by(sample) |>
-    dplyr::summarize(median_genes = median(feature))
-
-
+# Calculate median number of genes per sample
 count_genes <- dplyr::tibble(
     feature = sc_merge@meta.data$nFeature_RNA,
     sample = sc_merge@meta.data$sample
-)
+) |>
+dplyr::group_by(sample) |>
+dplyr::summarize(median_genes = median(feature))
 
-count_genes_plot <-
-    count_genes |>
+# Create a boxplot for the number of genes per nucleus
+count_genes_plot <- count_genes |>
     ggplot(aes(x = sample, y = feature, fill = sample)) +
     geom_boxplot() + 
     scale_fill_manual(values = my_cols_50) + 
@@ -35,6 +39,7 @@ count_genes_plot <-
     ylab("") + 
     ggtitle("Number of genes per nucleus")
 
+# Save the plot
 ggsave(
     file.path("results", "qc", "count_genes.pdf"),
     plot = count_genes_plot,
