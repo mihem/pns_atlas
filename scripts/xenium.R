359844d49be007fd680869a3c457ab0359f76831
mischko <mhem@mailbox.org>
2024-11-25 22:55:10 +0100

    document xenium.R

--- scripts/xenium.R
+++ scripts/xenium.R
@@ -1,3 +1,13 @@
+#===============================================================================
+# Xenium Data Analysis Pipeline
+#
+# Purpose: Process and analyze Xenium spatial transcriptomics data by:
+# 1. Loading and preprocessing Xenium data
+# 2. Integrating with single-nucleus data for cell type prediction
+# 3. Generating spatial visualization plots
+# 4. Analyzing marker abundance
+#===============================================================================
+
 # analyze xenium data
 
 # load libraries
@@ -7,11 +17,8 @@ library(scMisc)
 library(sf)
 future::plan("multicore", workers = 6)
 
-# load Xenium data
+# load Xenium data ----
 xenium_paths <- list.dirs(file.path("xenium", "raw"), full.names = TRUE, recursive = FALSE)
-
-# Get the center position of each centroid. There is one row per cell in this dataframe.
-str(GetTissueCoordinates(xenium_s11[["fov"]][["centroids"]]))
 
 xenium_meta <- 
     readxl::read_excel(file.path("lookup", "xenium_meta.xlsx"))  |>
@@ -43,52 +50,15 @@ xenium_objects <- lapply(
 ) |>
     setNames(xenium_names)
 
+# Load and preprocess Xenium data
+# - Reads Xenium data from raw files
+# - Filters cells with < 10 counts
+# - Performs SCTransform normalization
+
 # filter cells with less than 10 counts
 xenium_objects <- lapply(xenium_objects, function(x) {
     x <- subset(x, subset = nCount_Xenium > 9)
 })
-
-# # function to plot Xenium image plots for all samples
-# XeniumImagePlot <- function(xenium_objects, markers_xenium, sel_cluster) {
-#     molecules_sel <-
-#         markers_xenium |>
-#         dplyr::filter(cluster %in% {{ sel_cluster }}) |>
-#         pull(transcript)
-#     for (i in seq_along(xenium_objects)) {
-#         dir.create(file.path("results", "xenium", names(xenium_objects)[i]))
-#         plot <- ImageDimPlot(xenium_objects[[i]],
-#             fov = "fov",
-#             molecules = molecules_sel,
-#             nmols = 20000,
-#             cols = "white"
-#         )
-#         ggsave(
-#             plot = plot,
-#             filename = file.path("results", "xenium", names(xenium_objects)[i], paste0(sel_cluster, ".png")),
-#             width = 8,
-#             height = 8
-#         )
-#     }
-# }
-
-# markers_xenium <- read_xlsx(file.path("lookup", "xenium_list_final.xlsx"))
-
-# clusters_xenium <- unique(markers_xenium$cluster)
-
-# lapply(
-#     clusters_xenium,
-#     function(x) {
-#         XeniumImagePlot(xenium_objects = xenium_objects, markers_xenium = markers_xenium, sel_cluster = x)
-#     })
-
-
-# peric_mol <- c("SLC2A1", "KRT19", "FOXD1", "CLDN1", "CXCL14", "AQP1")
-
-# for (i in names(xenium_objects)) {
-#     p1 <- ImageDimPlot(xenium_objects[[i]], molecules = peric_mol, group.by = "orig.ident", cols = "white")
-#     ggsave(file.path("results", "xenium", "periC", paste0(i, ".png")), plot = p1, width = 15, height = 15)
-# }
-
 
 #integration with seurat
 for (xenium_object in (xenium_objects)) {
@@ -102,6 +72,10 @@ sc_merge_small <- subset(sc_merge, downsample = 1000)
 sc_merge_small <- SCTransform(sc_merge_small, verbose = FALSE)
 
 predictions <- list()
+
+# Integration with single-nucleus reference data
+# - Finds transfer anchors between Xenium and snRNA-seq data
+# - Transfers cell type labels from snRNA-seq to Xenium cells
 
 for (i in names(xenium_objects)) {
     anchor <- FindTransferAnchors(
@@ -149,6 +123,10 @@ for (i in names(xenium_objects)) {
     xenium_objects[[i]]$sn_predictions <- factor(xenium_objects[[i]]$sn_predictions, levels = xenium_objects[[i]]@misc$cluster_order)
 }
 
+# Visualization of spatial data
+# - Generates spatial plots colored by predicted cell types
+# - Creates cropped views of specific regions
+# - Plots different cell populations (SC, T_NK cells)
 
 for (i in names(xenium_objects)) {
     p1 <- ImageDimPlot(xenium_objects[[i]], group.by = "sn_predictions", cols = xenium_objects[[i]]@misc$cluster_col)
@@ -227,110 +205,6 @@ for (i in c("S24_CTRL")) {
 }
 
 
-# # predictions, periC only
-# for (i in names(xenium_objects)) {
-#     p1 <- ImageDimPlot(
-#         xenium_objects[[i]],
-#         cells = WhichCells(xenium_objects[[i]], idents = c("periC1", "periC2", "periC3")),
-#         group.by = "sn_predictions",
-#         cols = xenium_objects[[i]]@misc$cluster_col
-#     )
-#     ggsave(plot = p1, filename = file.path("results", "xenium", "periC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-# }
-
-# for (i in names(xenium_objects)) {
-#     p1 <- ImageDimPlot(xenium_objects[[i]], molecules = "TIMP1", group.by = "orig.ident", cols = "white")
-#     ggsave(plot = p1, filename = file.path("results", "xenium", "TIMP1", paste0(i, ".png")), width = 8, height = 8)
-# }
-
-# predictions, LEC only
-# for (i in names(xenium_objects)) {
-#     try({
-#     p1 <- ImageDimPlot(
-#         xenium_objects[[i]],
-#         cells = WhichCells(xenium_objects[[i]], idents = c("LEC")),
-#         group.by = "sn_predictions",
-#         cols = xenium_objects[[i]]@misc$cluster_col,
-#         size = 1
-#     );
-#     ggsave(plot = p1, filename = file.path("results", "xenium", "LEC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-# })
-# }
-
-
-# # predictions, IC only
-# for (i in names(xenium_objects)) {
-#     try(
-#         p1 <- ImageDimPlot(
-#             xenium_objects[[i]],
-#             cells = WhichCells(xenium_objects[[i]], idents = c("Macro1", "Macro2", "Granulo", "B", "T_NK", "Mast")),
-#             group.by = "sn_predictions",
-#             cols = pals::cols25(),
-#             # cols = xenium_objects[[i]]@misc$cluster_col,
-#             size = 1
-#         )
-#         ggsave(plot = p1, filename = file.path("results", "xenium", "IC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-#     )
-# }
-
-# # predictions, IC only
-
-# ic_clusters <- c("Macro1", "Macro2", "Granulo", "B", "T_NK", "Mast")
-# prediction_ic_col <- setNames(pals::cols25(length(ic_clusters)), ic_clusters)
-
-# for (i in names(xenium_objects)) {
-#     try({
-#         p1 <- ImageDimPlot(
-#             xenium_objects[[i]],
-#             cells = WhichCells(xenium_objects[[i]], idents = c("Macro1", "Macro2", "Granulo", "B", "T_NK", "Mast")),
-#             group.by = "sn_predictions",
-#             cols = prediction_ic_col,
-#             size = 1
-#         );
-#         ggsave(plot = p1, filename = file.path("results", "xenium", "IC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-#     })
-# }
-
-# # plot IC only, but those with missing clusters
-# for (i in c("S22_CTRL", "S04_CIAP")) {
-#     try({
-#         p1 <- ImageDimPlot(
-#             xenium_objects[[i]],
-#             cells = WhichCells(xenium_objects[[i]], idents = c("Macro2", "Granulo", "B", "T_NK", "Mast")),
-#             group.by = "sn_predictions",
-#             cols = prediction_ic_col,
-#             size = 1
-#         );
-#         ggsave(plot = p1, filename = file.path("results", "xenium", "IC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-#     })
-# }
-
-# for (i in c("S14_CIAP")) {
-#     try({
-#         p1 <- ImageDimPlot(
-#             xenium_objects[[i]],
-#             cells = WhichCells(xenium_objects[[i]], idents = c("Macro1", "Macro2", "Granulo","Mast")),
-#             group.by = "sn_predictions",
-#             cols = prediction_ic_col,
-#             size = 1
-#         );
-#         ggsave(plot = p1, filename = file.path("results", "xenium", "IC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-#     })
-# }
-
-# for (i in c("S24_CTRL")) {
-#     try({
-#         p1 <- ImageDimPlot(
-#             xenium_objects[[i]],
-#             cells = WhichCells(xenium_objects[[i]], idents = c("Macro2", "Granulo", "T_NK", "Mast")),
-#             group.by = "sn_predictions",
-#             cols = prediction_ic_col,
-#             size = 1
-#         );
-#         ggsave(plot = p1, filename = file.path("results", "xenium", "IC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-#     })
-# }
-
 # only T_NK
 for (i in names(xenium_objects)) {
     try({
@@ -347,6 +221,11 @@ for (i in names(xenium_objects)) {
         ggsave(plot = p1, filename = file.path("results", "xenium", "t_nk", paste0("seurat_predict_", i, ".png")), width = 8, height = 8, dpi = 1200)
     })
 }
+
+# Analysis of cell type abundances and marker quantification
+# - Summarizes predictions into broader cell type groups
+# - Analyzes manually quantified marker abundance
+# - Generates statistical plots and comparisons
 
 # summarize predictions to larger groups
 predictions_lookup <-
@@ -390,17 +269,6 @@ ggsave(plot = S24_group_plot, filename = file.path("results", "xenium", paste0("
 for (i in names(xenium_objects)) {
     Idents(xenium_objects[[i]]) <- xenium_objects[[i]]$sn_predictions_group
 }
-
-# # predictions, peri/epi/endoC only
-# for (i in names(xenium_objects)) {
-#     p1 <- ImageDimPlot(
-#         xenium_objects[[i]],
-#         cells = WhichCells(xenium_objects[[i]], idents = c("periC", "epiC", "endoC")),
-#         group.by = "sn_predictions_group",
-#         cols = prediction_group_col
-#     )
-#     ggsave(plot = p1, filename = file.path("results", "xenium", "periC_epiC_endoC", paste0("seurat_predict_", i, ".png")), width = 8, height = 8)
-# }
 
 qs::qsave(xenium_objects, file.path("objects", "xenium_objects.qs"))
 xenium_objects <- qs::qread(file.path("objects", "xenium_objects.qs"))
@@ -518,136 +386,8 @@ ggsave(file.path("results", "xenium", "abundance_xenium_sc_t_nk.pdf"),
 )
 
 
+# Export results ----
 # export prediction annotation for python analysis
 for (i in names(xenium_objects)) {
     arrow::write_parquet(xenium_objects[[i]]@meta.data, sink = file.path("results", "xenium", paste0("xenium_predictions_", i, ".parquet")))
 }
-
-# my_props <- speckle::getTransformedProps(
-#     clusters = cells_predicted$cluster,
-#     sample = cells_predicted$sample,
-#     transform = "logit"
-# )
-
-# meta_lookup <- 
-#     cells_predicted |>
-#     select(sample, condition) |>
-#     distinct()
-
-# propellerCalcXenium <- function(condition1, condition2, min_cells, formula, cl_interest = NULL) {
-#     if(is.null(cl_interest)) {
-#     cl_interest <-
-#         cells_predicted |>
-#         dplyr::count(cluster, condition) |>
-#         pivot_wider(names_from = condition, values_from = n) |>
-#         mutate(group_sum = .data[[condition1]] + .data[[condition2]]) |>
-#         dplyr::filter(group_sum > min_cells) |>
-#         pull(cluster)
-#     }
-#     my_design <- model.matrix(as.formula(formula), data = meta_lookup)
-#     my_contrasts <- glue::glue("condition{condition1}-condition{condition2}")
-#     my_args <- list(my_contrasts, levels = my_design)
-#     my_contr <- do.call(limma::makeContrasts, my_args)
-#     propeller_result <-
-#         speckle::propeller.ttest(prop.list = my_props, design = my_design, contrast = my_contr, robust = TRUE, trend = FALSE, sort = TRUE) |>
-#         tibble::rownames_to_column("cluster") |>
-#         dplyr::filter(cluster %in% cl_interest) |>
-#         dplyr::mutate(log2ratio = log2(PropRatio)) |>
-#         dplyr::mutate(FDR_log = -log10(FDR)) |>
-#         tibble::tibble()
-#     return(propeller_result)
-# }
-
-# propeller_VN_CTRL <- propellerCalcXenium(
-#     condition1 = "VN",
-#     condition2 = "CTRL",
-#     min_cells = 30,
-#     formula = "~0 + condition",
-#     cl_interest = c("mySC", "nmSC", "repairSC")
-# )
-
-# scMisc::dotplotPropeller(
-#     data = propeller_VN_CTRL,
-#     color = sc_merge@misc$cluster_col,
-#     filename = "Xenium_SC_VN_CTRL",
-#     width = 5,
-#     height = 2.5
-# )
-
-# # deconvolution using spacexr
-## comment: did not work as well as Integration with seurat, only few cells mapped
-# xenium_s11 <- LoadXenium(xenium_paths[4])
-# remotes::install_github("dmcable/spacexr")
-
-# library(spacexr)
-
-
-
-# query.counts <- GetAssayData(xenium_s11, assay = "Xenium", slot = "counts")
-# coords <- GetTissueCoordinates(xenium_s11, which = "centroids")
-# rownames(coords) <- coords$cell
-# coords$cell <- NULL
-# query <- SpatialRNA(coords, query.counts, colSums(query.counts))
-
-# sc_merge_small <- subset(sc_merge, downsample = 1000)
-# table(sc_merge_small$cluster)
-
-# Idents(sc_xenium) <- sc_xenium$cluster
-# sc_xenium_small <- subset(sc_xenium, downsample = 1000)
-# #remove macro1 becuase less than 25 cells
-# sc_xenium_small <- subset(sc_xenium_small, cluster %in% c("Macro1"), invert = TRUE)
-# sc_xenium_small$cluster <- droplevels(sc_xenium_small$cluster)
-
-
-# s11 <- subset(sc_xenium, subset = sample %in% c("S11"))
-# #remove small clusters
-# s11 <- subset(s11, cluster %in% c("damageSC", "periC3", "VSMC", "venEC", "Macro1", "Granulo", "B"), invert = TRUE)
-# s11$cluster <- droplevels(s11$cluster)
-
-# seurat_obj <- s11
-# seurat_obj <- sc_merge_small
-
-# dplyr::count(seurat_obj@meta.data, cluster, sort = TRUE)
-
-# counts <- GetAssayData(seurat_obj, assay = "RNA", slot = "counts")
-# cluster <- seurat_obj$cluster
-# nUMI <- seurat_obj$nCount_RNA
-# reference <- Reference(counts, cluster, nUMI)
-
-# # run RCTD with many cores (~10 min)
-# RCTD <- create.RCTD(query, reference, max_cores = 4)
-# RCTD <- run.RCTD(RCTD, doublet_mode = "doublet")
-
-# annotations.df <- RCTD@results$results_df
-
-# dplyr::count(annotations.df, first_type, sort = TRUE)
-# annotations <- annotations.df$first_type
-# names(annotations) <- rownames(annotations.df)
-# xenium.obj$predicted.celltype <- annotations
-
-# dplyr::count(xenium.obj@meta.data, predicted.celltype, sort = TRUE)
-# qs::qsave(xenium.obj, file.path("objects", "xenium_s11_rctd.qs"))
-# xenium.obj <- qs::qread(file.path("objects", "xenium_s11_rctd.qs"))
-
-# keep.cells <- Cells(xenium.obj)[!is.na(xenium.obj$predicted.celltype)]
-# xenium.obj <- subset(xenium.obj, cells = keep.cells)
-
-# p1 <- ImageDimPlot(xenium.obj, group.by = "predicted.celltype", cols = "polychrome")
-# ggsave(plot = p1, filename = "test.png")
-
-# # does not work, probably too few cells predicted
-# # # niche assay
-# # xenium.obj <- BuildNicheAssay(
-# #     object = xenium.obj,
-# #     fov = "fov",
-# #     # group.by = "predicted.celltype",
-# #     niches.k = 5,
-# #     neighbors.k = 30
-# # )
-
-# # celltype.plot <- ImageDimPlot(xenium.obj, group.by = "predicted.celltype", size = 1.5, cols = "polychrome",
-# #     dark.background = F) + ggtitle("Cell type")
-# # niche.plot <- ImageDimPlot(xenium.obj, group.by = "niches", size = 1.5, dark.background = F) + ggtitle("Niches") +
-# #     scale_fill_manual(values = c("#442288", "#6CA2EA", "#B5D33D", "#FED23F", "#EB7D5B"))
-# # celltype.plot | niche.plot
-
