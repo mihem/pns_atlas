1c0faaf1e0ce6b2248ed6936814d1ddd26dd39a8
mischko <mhem@mailbox.org>
2024-11-25 20:07:14 +0100

    document cerebro.R

--- scripts/cerebro.R
+++ scripts/cerebro.R
@@ -1,9 +1,17 @@
-# prepare data for cerebroApp
+#===============================================================================
+# Cerebro Export Script
+#===============================================================================
+# Purpose: Prepare and export Seurat object for CerebroApp visualization:
+# - Convert Seurat object to Cerebro-compatible format
+# - Clean up unnecessary data and optimize memory usage
+# - Export selected metadata and marker genes
+# - Generate interactive visualization file
+#===============================================================================
 
-# install my version of cerebroApp
+# Install custom cerebroApp version ----
 remotes::install_github("mihem/cerebroAppLite")
 
-# load libraries
+# load libraries ----
 library(Seurat)
 library(BPCells)
 library(SeuratObject)
@@ -12,20 +20,20 @@ library(conflicted)
 library(qs)
 library(cerebroAppLite)
 
-# general settings  ----
+# general settings ----
 options(warn = 0) 
 conflicts_prefer(base::setdiff)
 
 # load preprocessed data ----
 sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"), nthread = 4)
 
-# downsample ----
+# prepare data for cerebro ----
+# Downsample and clean up Seurat object
 sc_merge_small <- subset(sc_merge, downsample = 1000)
 
-##export from seurat to cerebro format
 sc_merge_cerebro <- sc_merge
 
-# remove unnecessary assays and dims
+# Remove unnecessary data to optimize memory usage
 sc_merge_cerebro <- DietSeurat(
     sc_merge_cerebro,
     layers = c("data"),
@@ -33,13 +41,9 @@ sc_merge_cerebro <- DietSeurat(
     dimreducs = c("umap.scvi.full")
 )
 
-str(sc_merge_cerebro$RNA$counts@matrix, max.level = 3)
-
-# somehow the scale.data is still present, only data required for gene expression
+# Additional cleanup
 sc_merge_cerebro[["RNA"]]$scale.data <- NULL
 sc_merge_cerebro[["RNA"]]$counts <- NULL
-
-# write_matrix_dir(sc_merge_cerebro$RNA$data, dir = paste0("matrix_cerebro/"))
 
 # # convert to in memory matrix
 # sc_merge_cerebro[["RNA"]]$counts <- as(object = sc_merge_cerebro[["RNA"]]$counts, Class = "dgCMatrix")
@@ -47,7 +51,9 @@ sc_merge_cerebro[["RNA"]]$counts <- NULL
 # # convert to v3 assay
 # sc_merge_cerebro[["RNA"]] <- as(object = sc_merge_cerebro[["RNA"]], Class = "Assay")
 
-# only keep relevant metadata
+
+# Prepare metadata for export ----
+# Select and rename relevant metadata columns
 sc_merge_cerebro@meta.data <-
     sc_merge_cerebro@meta.data |>
     tibble::rownames_to_column("barcode") |>
@@ -60,6 +66,7 @@ sc_merge_cerebro@meta.data <-
     select(barcode, cluster, group, diagnosis, nCount_RNA, nFeature_RNA, percent_mt, sample, sex, age, center, milbrandt_sciatic, suter_p60) |>
     tibble::column_to_rownames(var = "barcode")
 
+# Calculate marker genes ----
 sc_merge_cerebro <- getMarkerGenes(
   sc_merge_cerebro,
   organism = "hg",
@@ -71,6 +78,8 @@ sc_merge_cerebro <- getMarkerGenes(
   thres_p_val = 0.05
 )
 
+# Export for CerebroApp ----
+# Save processed object and export to Cerebro format
 qs::qsave(sc_merge_cerebro, file = file.path("objects", "sc_merge_cerebro.qs"))
 
 cerebroAppLite::exportFromSeurat(
@@ -84,4 +93,5 @@ cerebroAppLite::exportFromSeurat(
   use_delayed_array = FALSE
 )
 
+# Launch CerebroApp ----
 cerebroAppLite::launchCerebro()
