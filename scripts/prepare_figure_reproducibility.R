0df3f90dd87ee590b2218257a8d9f2b12d12315b
mischko <mhem@mailbox.org>
2024-12-01 18:55:37 +0100

    add repro Fig 3A

--- scripts/prepare_figure_reproducibility.R
+++ scripts/prepare_figure_reproducibility.R
@@ -49,6 +49,7 @@ qs::qsave(umap_figure, file.path("docs", "umap_figure.qs"))
 # Figure 2 ----
 ic <- qs::qread(file.path("objects", "ic.qs"), nthread = 4)
 
+# Remove unnecessary data to further reduce the object size
 ic_figure <- DietSeurat(
     ic,
     counts = TRUE,
@@ -58,7 +59,6 @@ ic_figure <- DietSeurat(
     dimreducs = c("umap.rpca")
 )
 
-# Remove unnecessary data to further reduce the object size
 ic_figure$RNA$counts <- NULL
 ic_figure$RNA$scale.data <- NULL
 
@@ -73,8 +73,10 @@ ic_figure@tools <- list()
 qsave(ic_figure, file.path("docs", "ic_figure.qs"))
 ic_figure <- qread(file.path("docs", "ic_figure.qs"))
 
+# subset the object to only include the B and Plasma clusters
 b_plasma <- subset(ic, idents = c("Plasma", "B"))
 
+# Remove unnecessary data to further reduce the object size
 b_plasma_figure <- DietSeurat(
     b_plasma,
     counts = TRUE,
@@ -88,9 +90,6 @@ b_plasma_figure@meta.data <-
     b_plasma_figure@meta.data |>
     dplyr::select(ic_cluster)
 
-str(b_plasma_figure@meta.data$ic_cluster)
-
-
 b_plasma_figure$RNA$counts <- NULL
 b_plasma_figure$RNA$scale.data <- NULL
 b_plasma_figure@commands <- list()
@@ -98,5 +97,39 @@ b_plasma_figure@tools <- list()
 
 qsave(b_plasma_figure, file.path("docs", "b_plasma_figure.qs"))
 
+# save enrichment results as qs object
 enrichr_macro18 <- readxl::read_excel(file.path("results", "enrichr", "enrichr_Macro18.xlsx"), sheet = "GO_Biological_Process_2023")
 qsave(enrichr_macro18, file.path("docs", "enrichr_macro18.qs"))
+
+# Figure 3 ----
+propeller_PNP_CTRL <-
+    scMisc::propellerCalc(
+        seu_obj1 = sc_merge,
+        condition1 = "PNP",
+        condition2 = "CTRL",
+        cluster_col = "cluster",
+        meta_col = "level0",
+        lookup = sample_lookup,
+        sample_col = "sample",
+        formula = "~0 + level0",
+        min_cells = 30
+    ) |>
+    dplyr::filter(abs(log2ratio) > 0.5)
+
+qsave(propeller_PNP_CTRL, file.path("docs", "propeller_PNP_CTRL.qs"))
+
+propeller_PNP_CTRL_ic <-
+    scMisc::propellerCalc(
+        seu_obj1 = ic,
+        condition1 = "PNP",
+        condition2 = "CTRL",
+        cluster_col = "ic_cluster",
+        meta_col = "level0",
+        lookup = sample_lookup,
+        sample_col = "sample",
+        formula = "~0 + level0",
+        min_cells = 30
+    ) |>
+    dplyr::filter(abs(log2ratio) > 0.5)
+
+qsave(propeller_PNP_CTRL_ic, file.path("docs", "propeller_PNP_CTRL_ic.qs"))
