51d7cb5a29f3168b91c4c9fba5274f292de11c70
mischko <mhem@mailbox.org>
2024-12-08 12:22:15 +0100

    add macro18 samc module plot

--- scripts/immune_cells.R
+++ scripts/immune_cells.R
@@ -1,6 +1,6 @@
-#===============================================================================
+# ===============================================================================
 # Immune Cell Analysis Script
-#===============================================================================
+# ===============================================================================
 # Purpose: Characterize and annotate immune cell populations from snRNA-seq data
 #
 # Key Analysis Steps:
@@ -10,7 +10,7 @@
 # 4. Clustering optimization
 # 5. Manual annotation based on markers and reference mapping
 #
-#===============================================================================
+# ===============================================================================
 
 # Load required libraries and set up environment ----
 library(Seurat)
@@ -45,20 +45,20 @@ ic$cluster <- droplevels(ic$cluster)
 
 # Reference mapping with Azimuth PBMC reference ----
 # run Azimuth mapping to references ----
-# Map to PBMC reference 
+# Map to PBMC reference
 ic_azimuth <- RunAzimuth(query = ic, reference = "pbmcref", assay = "RNA")
 qsave(ic_azimuth, file.path("objects", "ic_azimuth.qs"))
 
 # prepare predictions ----
 predictions_pbmcref <-
-    data.frame(
-        ic_level1 = ic_azimuth$predicted.celltype.l1,
-        ic_level1_score = ic_azimuth$predicted.celltype.l1.score,
-        ic_level2 = ic_azimuth$predicted.celltype.l2,
-        ic_level2_score = ic_azimuth$predicted.celltype.l2.score,
-        ic_level3 = ic_azimuth$predicted.celltype.l3,
-        ic_level3_score = ic_azimuth$predicted.celltype.l3.score
-    )|>
+  data.frame(
+    ic_level1 = ic_azimuth$predicted.celltype.l1,
+    ic_level1_score = ic_azimuth$predicted.celltype.l1.score,
+    ic_level2 = ic_azimuth$predicted.celltype.l2,
+    ic_level2_score = ic_azimuth$predicted.celltype.l2.score,
+    ic_level3 = ic_azimuth$predicted.celltype.l3,
+    ic_level3_score = ic_azimuth$predicted.celltype.l3.score
+  ) |>
   mutate(ic_level1 = ifelse(ic_level1_score < 0.4, "unknown", ic_level1)) |>
   mutate(ic_level2 = ifelse(ic_level2_score < 0.4, "unknown", ic_level2)) |>
   mutate(ic_level3 = ifelse(ic_level3_score < 0.4, "unknown", ic_level3))
@@ -68,7 +68,7 @@ ic <- AddMetaData(ic, predictions_pbmcref)
 
 # plot predictions ----
 pred_plot_ic_lev2 <-
- DimPlot(ic, reduction = "umap.scvi.full", group.by = "ic_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = rev(my_cols_25), label = FALSE )+
+  DimPlot(ic, reduction = "umap.scvi.full", group.by = "ic_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = rev(my_cols_25), label = FALSE) +
   theme_rect()
 
 ggsave(plot = pred_plot_ic_lev2, file.path("results", "map", "map_ic_lev2.png"), width = 15, height = 10)
@@ -88,7 +88,8 @@ ic_slim[["RNA"]]$scale.data <- NULL
 # Perform multi-modal integration ----
 ic_slim[["RNA"]] <- split(
   x = ic_slim[["RNA"]],
-  f = ic_slim$sample)
+  f = ic_slim$sample
+)
 
 ic_slim <-
   ic_slim |>
@@ -133,17 +134,17 @@ qsave(ic_slim, file.path("objects", "ic_reclustered.qs"))
 # plot umaps ----
 # rpca looked best
 umap_ic_map <-
-    DimPlot(ic, reduction = "umap.rpca", pt.size = .5, raster = FALSE, alpha = 0.1, group.by = "ic_level2", cols = my_cols_25, label = TRUE) +
-    theme_rect() + 
-    xlab("UMAP1") + 
-    ylab("UMAP2")
+  DimPlot(ic, reduction = "umap.rpca", pt.size = .5, raster = FALSE, alpha = 0.1, group.by = "ic_level2", cols = my_cols_25, label = TRUE) +
+  theme_rect() +
+  xlab("UMAP1") +
+  ylab("UMAP2")
 
 ggsave(plot = umap_ic_map, file.path("results", "map", "ic_azimuth_rpca.png"), width = 10, height = 8)
 
 Idents(ic_slim) <- ic_slim$ic_level2
 umap_ic_map_split <-
-    DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .5, raster = FALSE, alpha = 0.1, split.by = "ic_level2", cols = my_cols_25, label = TRUE) +
-    theme_rect()
+  DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .5, raster = FALSE, alpha = 0.1, split.by = "ic_level2", cols = my_cols_25, label = TRUE) +
+  theme_rect()
 
 ggsave(plot = umap_ic_map_split, file.path("results", "map", "ic_azimuth_rpca_split.png"), width = 40, height = 8)
 
@@ -161,18 +162,18 @@ resolutions <- paste0("RNA_snn_res.", seq(from = 0.2, to = 2.3, by = 0.1))
 
 umap_list <- list()
 for (res in resolutions) {
-    umap_list[[res]] <-
-        DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.1, group.by = res, cols = my_cols_50, label = TRUE) +
-        theme_rect() +
-        NoLegend()
+  umap_list[[res]] <-
+    DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.1, group.by = res, cols = my_cols_50, label = TRUE) +
+    theme_rect() +
+    NoLegend()
 }
 
 umap_list <- patchwork::wrap_plots(umap_list, ncol = 2)
 ggsave(plot = umap_list, file.path("results", "umap", "ic_rpca_umap_resolutions.png"), width = 16, height = 40)
 
 umap_ic <-
-    DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.1, group.by = "RNA_snn_res.2.3", cols = my_cols_50, label = TRUE) +
-    theme_rect()
+  DimPlot(ic_slim, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.1, group.by = "RNA_snn_res.2.3", cols = my_cols_50, label = TRUE) +
+  theme_rect()
 
 ggsave(plot = umap_ic, file.path("results", "umap", "ic_rpca_umap.png"), width = 10, height = 8)
 
@@ -204,7 +205,7 @@ endo_epi_macs <-
 ggsave(file.path("results", "featureplot", "ic_endo_epi_macs.png"), endo_epi_macs, width = 8, height = 6)
 ggsave(file.path("results", "featureplot", "ic_endo_epi_macs.pdf"), endo_epi_macs, width = 8, height = 6)
 
-b_igh <- 
+b_igh <-
   FeaturePlot(
     ic,
     features = c("IGHM", "IGHD", "IGHG1", "IGHG2", "IGHG3", "IGHG4", "IGHA1", "IGHA2"),
@@ -256,7 +257,7 @@ dotPlot(
   dot_min = 0.01,
   height = 8,
   width = 6
-) 
+)
 
 dotPlot(
   path = file.path("lookup", "markers.csv"),
@@ -265,7 +266,7 @@ dotPlot(
   dot_min = 0.01,
   height = 7.5,
   width = 12
-) 
+)
 
 dotPlot(
   path = file.path("lookup", "markers.csv"),
@@ -274,7 +275,7 @@ dotPlot(
   dot_min = 0.01,
   height = 7.5,
   width = 20
-) 
+)
 
 # find markers ---
 ic_slim <- JoinLayers(ic_slim)
@@ -324,7 +325,7 @@ mapSeurat <- function(ref, query) {
 predictions_prev <- mapSeurat(ref = ts_blood, query = ic_slim)
 predictions_prev <- mapSeurat(ref = ts_bm, query = ic_slim)
 
-#function to store predictions in seurat object
+# function to store predictions in seurat object
 storePred <- function(predictions, label_col, score_col, seu_obj) {
   predictions_prep <-
     predictions |>
@@ -351,20 +352,20 @@ ic_slim <- storePred(predictions_prev, label_col = "ts_bm_label", score_col = "t
 str(ic_slim@meta.data)
 
 pred_plot_ts_blood <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "ts_blood_label", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "ts_blood_label", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_ts_blood, file.path("results", "map", "map_ic_ts_blood.png"), width = 20, height = 7)
 
 pred_plot_ts_bm <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "ts_bm_label", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "ts_bm_label", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_ts_bm, file.path("results", "map", "map_ic_ts_bm.png"), width = 20, height = 7)
 
 # run Azimuth bone marrow ---
 options(timeout = 600)
 ic_slim_bm <- RunAzimuth(query = ic_slim, reference = "bonemarrowref", assay = "RNA")
 
-# prepare predictions 
+# prepare predictions
 predictions_bm <-
   data.frame(
     bm_level1 = ic_slim_bm$predicted.celltype.l1,
@@ -375,7 +376,7 @@ predictions_bm <-
   mutate(bm_level1 = ifelse(bm_level1_score < 0.4, "unknown", bm_level1)) |>
   mutate(bm_level2 = ifelse(bm_level2_score < 0.4, "unknown", bm_level2))
 
-# add to seurat object 
+# add to seurat object
 ic_slim <- AddMetaData(ic_slim, predictions_bm)
 
 # save object
@@ -384,14 +385,14 @@ qsave(ic_slim_bm, file.path("objects", "ic_slim_bm.qs"))
 
 # plot predictions
 pred_plot_azimuth_bm_level2 <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "bm_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "bm_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_azimuth_bm_level2, file.path("results", "map", "map_ic_azimuth_bm_level2.png"), width = 20, height = 7)
 
 # run Azimuth tonsil ---
 ic_slim_tonsil <- RunAzimuth(query = ic_slim, reference = "tonsilref", assay = "RNA")
 
-# prepare predictions 
+# prepare predictions
 predictions_tonsil <-
   data.frame(
     tonsil_level1 = ic_slim_tonsil$predicted.celltype.l1,
@@ -402,7 +403,7 @@ predictions_tonsil <-
   mutate(tonsil_level1 = ifelse(tonsil_level1_score < 0.4, "unknown", tonsil_level1)) |>
   mutate(tonsil_level2 = ifelse(tonsil_level2_score < 0.4, "unknown", tonsil_level2))
 
-# add to seurat object 
+# add to seurat object
 ic_slim <- AddMetaData(ic_slim, predictions_tonsil)
 
 # save object
@@ -411,13 +412,13 @@ qsave(ic_slim_tonsil, file.path("objects", "ic_slim_tonsil.qs"))
 
 # plot predictions
 pred_plot_azimuth_tonsil_level1 <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "tonsil_level1", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "tonsil_level1", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_azimuth_tonsil_level1, file.path("results", "map", "map_ic_azimuth_tonsil_level1.png"), width = 20, height = 7)
 
 pred_plot_azimuth_tonsil_level2 <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "tonsil_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_50, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "tonsil_level2", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_50, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_azimuth_tonsil_level2, file.path("results", "map", "map_ic_azimuth_tonsil_level2.png"), width = 20, height = 7)
 
 # run Azimuth lung ---
@@ -425,7 +426,7 @@ ic_slim_lung <- RunAzimuth(query = ic_slim, reference = "lungref", assay = "RNA"
 
 str(ic_slim_lung@meta.data)
 
-# prepare predictions 
+# prepare predictions
 predictions_lung <-
   data.frame(
     lung_level1 = ic_slim_lung$predicted.ann_level_1,
@@ -434,12 +435,12 @@ predictions_lung <-
     lung_level2_score = ic_slim_lung$predicted.ann_level_2.score,
     lung_level3 = ic_slim_lung$predicted.ann_level_3,
     lung_level3_score = ic_slim_lung$predicted.ann_level_3.score
-  )   |>
+  ) |>
   mutate(lung_level1 = ifelse(lung_level1_score < 0.4, "unknown", lung_level1)) |>
   mutate(lung_level2 = ifelse(lung_level2_score < 0.4, "unknown", lung_level2)) |>
   mutate(lung_level3 = ifelse(lung_level3_score < 0.4, "unknown", lung_level3))
 
-# add to seurat object 
+# add to seurat object
 ic_slim <- AddMetaData(ic_slim, predictions_lung)
 
 # save object
@@ -448,8 +449,8 @@ qsave(ic_slim_lung, file.path("objects", "ic_slim_lung.qs"))
 
 # plot predictions
 pred_plot_azimuth_lung_level3 <-
- DimPlot(ic_slim, reduction = "umap.rpca", group.by = "lung_level3", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
-  theme_rect() 
+  DimPlot(ic_slim, reduction = "umap.rpca", group.by = "lung_level3", raster = FALSE, pt.size = .1, alpha = .1, cols = my_cols_25, label = TRUE) +
+  theme_rect()
 ggsave(plot = pred_plot_azimuth_lung_level3, file.path("results", "map", "map_ic_azimuth_lung_level3.png"), width = 20, height = 7)
 
 # Final cluster annotation ----
@@ -458,7 +459,7 @@ ggsave(plot = pred_plot_azimuth_lung_level3, file.path("results", "map", "map_ic
 Idents(ic_slim) <- ic_slim$RNA_snn_res.2.3
 
 annotations <- readxl::read_xlsx(file.path("lookup", "ic_cluster_annotation.xlsx")) |>
-    mutate(cluster = as.character(cluster)) 
+  mutate(cluster = as.character(cluster))
 
 names(annotations$final) <- levels(ic_slim)
 ic_slim <- RenameIdents(ic_slim, annotations$final)
@@ -467,7 +468,7 @@ ic_slim <- RenameIdents(ic_slim, annotations$final)
 ic_slim@misc$ic_cluster_order <- annotations$cluster_order[!is.na(annotations$cluster_order)]
 ic_slim@misc$ic_cluster_col <- setNames(my_cols_50[1:length(ic_slim@misc$ic_cluster_order)], ic_slim@misc$ic_cluster_order)
 
-#sanity check
+# sanity check
 setdiff(ic_slim@misc$ic_cluster_order, levels(ic_slim))
 setdiff(levels(ic_slim), ic_slim@misc$ic_cluster_order)
 any(duplicated(ic_slim@misc$ic_cluster_order))
@@ -479,13 +480,67 @@ ic$ic_cluster <- factor(Idents(ic), levels = ic@misc$ic_cluster_order)
 Idents(ic) <- ic$ic_cluster
 
 umap_ic <-
-    DimPlot(ic, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.3, group.by = "ic_cluster", cols = ic@misc$ic_cluster_col, label = TRUE) +
-    theme_rect() +
-    NoLegend() + 
-    xlab("UMAP1") +
-    ylab("UMAP2")
+  DimPlot(ic, reduction = "umap.rpca", pt.size = .1, raster = FALSE, alpha = 0.3, group.by = "ic_cluster", cols = ic@misc$ic_cluster_col, label = TRUE) +
+  theme_rect() +
+  NoLegend() +
+  xlab("UMAP1") +
+  ylab("UMAP2")
 
 ggsave(plot = umap_ic, file.path("results", "umap", "ic_rpca_annotated.png"), width = 8, height = 7)
 
 # Save final annotated object ----
 qs::qsave(ic, file.path("objects", "ic.qs"))
+
+# compare macro18 markers with samc ---
+macro18_markers <- findMarkers(ident1 = "Macro18", object = ic, only_pos = TRUE, min_pct = 0.1, logfc_threshold = 0.25, assay = "RNA")
+
+# load data from stroke project
+stroke <- qs::qread(file.path("objects", "stroke.qs"))
+
+# need to normalize RNA assay
+DefaultAssay(stroke) <- "RNA"
+stroke <- NormalizeData(stroke)
+
+# use same settings for samc markers
+samc_markers <- findMarkers(ident1 = "SAMC", object = stroke, only_pos = TRUE, min_pct = 0.1, logfc_threshold = 0.25, assay = "RNA")
+
+lookup_samc_markers <- homologene::mouse2human(samc_markers$gene)
+
+samc_markers_ortho <-
+  samc_markers |>
+  left_join(lookup_samc_markers, by = c("gene" = "mouseGene"))
+
+samc_top <-
+  samc_markers_ortho |>
+  slice(1:100) |>
+  dplyr::filter(humanGene %in% rownames(ic)) |>
+  pull(humanGene) |>
+  na.omit()
+
+ic <- AddModuleScore(
+  object = ic,
+  features = list(samc_top),
+  name = "SAMC"
+)
+
+# better name
+ic$SAMC <- ic$SAMC1
+ic$SAMC1 <- NULL
+
+fplot_samc <-
+  FeaturePlot(
+    ic,
+    features = "SAMC",
+    reduction = "umap.rpca",
+    pt.size = 0.1,
+    raster = FALSE,
+    label = TRUE,
+    coord.fixed = TRUE,
+    order = TRUE
+  ) +
+    viridis::scale_color_viridis() +
+    theme_rect() +
+    xlab("UMAP1") &
+    ylab("UMAP2")
+
+ggsave(file.path("results", "module", "ic_samc.png"), fplot_samc, width = 8, height = 6)
