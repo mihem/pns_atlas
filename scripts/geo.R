bad1a6a056f1f1b3ec782a2a944b72df2bc1cf61
mischko <mhem@mailbox.org>
2024-12-29 21:27:06 +0100

    add geo script

---
+++ scripts/geo.R
@@ -0,0 +1,39 @@
+
+#===============================================================================
+# GEO submission
+#
+# Purpose: Export metadata for GEO submission
+#===============================================================================
+
+# Load required libraries ----
+library(Seurat)
+library(qs)
+library(tibble)
+
+# Data preparation ----
+# Load preprocessed data and calculate age for each sample
+sc_merge <- qs::qread(file.path("objects", "sc_merge.qs"), nthread = 4)
+ic <- qs::qread(file.path("objects", "ic.qs"), nthread = 4)
+
+str(sc_merge@meta.data)
+
+geo_meta_all <- 
+    sc_merge@meta.data |>
+    tibble::rownames_to_column("barcode") |>
+    dplyr::select(
+      barcode,
+      sample,
+      cluster,
+      level2,
+      nCount_RNA:incat,
+      g_ratio,
+      axon_diameter,
+      milbrandt_sciatic_label_full,
+      milbrandt_sciatic_label_full.score,
+      suter_p60_label_full,
+      suter_p60_label_full.score,
+      rosmap_label,
+      rosmap_score
+    )
+
+write.csv(geo_meta_all, file = file.path("geo", "metadata_all.csv"), row.names = FALSE)
